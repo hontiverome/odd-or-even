a1b2eb5f41f0e58560b571690f6fee89a60ced2a
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-22 22:55:41 +0800

    added pseudocode

--- odd-or-even.py
+++ odd-or-even.py
@@ -2,3 +2,6 @@
 # The program will create two other text file; 
 # The first text file will be named even.txt that will contain all even numbers extracted from the numbers.txt
 # The second text file will be named odd.txt that will contain all odd numbers extracted from the numbers.txt
+
+# read and write the input and output files
+# put the numbers from the read file into a list and convert it into integers 
