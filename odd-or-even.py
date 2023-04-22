c3d4413daaa6a6b1b8b9c5721f9ec9964ffc9a03
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-22 22:30:17 +0800

    added directions

---
+++ odd-or-even.py
@@ -0,0 +1,4 @@
+# Write a python program that reads a text file named numbers.txt that contain 20 integers.
+# The program will create two other text file; 
+# The first text file will be named even.txt that will contain all even numbers extracted from the numbers.txt
+# The second text file will be named odd.txt that will contain all odd numbers extracted from the numbers.txt
