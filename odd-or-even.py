87b2f184fee7173491e98031d772e2ca2ba633c0
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-22 23:01:53 +0800

    added .txts

--- odd-or-even.py
+++ odd-or-even.py
@@ -4,6 +4,6 @@
 # The second text file will be named odd.txt that will contain all odd numbers extracted from the numbers.txt
 
 # read and write the input and output files
-with open("numbers.txt", "r") as number_file, open ("even", "w") as even_file, open ("odd", "w") as odd_file:
+with open("numbers.txt", "r") as number_file, open ("even.txt", "w") as even_file, open ("odd.txt", "w") as odd_file:
     
 # put the numbers from the read file into a list and convert it into integers
