b88611fdf45b16553855b93498df6ee01ee29596
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 18:51:31 +0800

    troubleshooting

--- odd-or-even.py
+++ odd-or-even.py
@@ -10,7 +10,6 @@ with open("numbers.txt", "r") as integer_file, open ("even.txt", "w") as even_fi
         if contents=='y':
             for line in integer_file:
                 print(line.strip().rjust(25)+'\n')
-                break
             break
         elif contents=='n':
             break
@@ -19,7 +18,7 @@ with open("numbers.txt", "r") as integer_file, open ("even.txt", "w") as even_fi
             contents=str(input("Would you like to see the contents of 'numbers.txt?' (y or n)"))
             continue
     # put the numbers from the read file into a list and convert it into integers
-    integers=[line.strip() for line in integer_file]
+    integers=[lines.strip() for lines in integer_file]
     integer_list=[int(j) for j in integers]
     # iterate the list
     for num in integer_list:
