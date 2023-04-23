fb2eff186cb527b51ef4446965f18a60e0af0446
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 13:35:49 +0800

    added more pseudocodes

--- odd-or-even.py
+++ odd-or-even.py
@@ -9,4 +9,6 @@ with open("numbers.txt", "r") as integer_file, open ("even.txt", "w") as even_fi
     # put the numbers from the read file into a list and convert it into integers
     integers=[line.strip() for line in integer_file]
     integer_list=[int(j) for j in integers]
-    print(integer_list)
+    # identify whether integer is even or odd using mod
+    # if odd, write it to "odd.txt" file
+    # if even, write it to "even.txt" file
